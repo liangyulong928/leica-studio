395251da2efa402b22ec065b7ee732d3f59c43f0
yulong <liangyulong928@qq.com>
2022-11-10 15:18:41 +0800

    增加登录界面和注册界面的前端渲染

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,10 @@
 class IndexController < ApplicationController
   def homepage
   end
+
+  def login
+  end
+
+  def register
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'opus/details'
   get 'index/homepage'
+  get 'index/login'
+  get 'index/register'
+
+  root "index#homepage"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
